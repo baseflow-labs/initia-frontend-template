0b61765043476eb298363a228a3c8c0db655b97a
Makkahwi <SuhaibAhmadAi@hotmail.com>
2025-05-08 14:04:28 +0300

    Setup Localization Infrastructure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { useTranslation } from "react-i18next";
+import "./i18next";
+
 const App = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="text-success">
-      <h1>This is Mustaheq</h1>
+      <h1>{t("Home.Welcome")}</h1>
+      <h1>{t("Home.Welcomes")}</h1>
     </div>
   );
 };
---
+++ src/i18next.ts
@@ -0,0 +1,38 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import ar from "./assets/locales/ar.json";
+import en from "./assets/locales/en.json";
+
+// don't want to use this?
+// have a look at the Quick start guide
+// for passing in lng and translations on init
+
+i18n
+  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  // .use(LanguageDetector)
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
+  .init({
+    resources: {
+      en: {
+        translations: en,
+      },
+      ar: {
+        translations: ar,
+      },
+    },
+    fallbackLng: "ar",
+    debug: false,
+    ns: ["translations"],
+    defaultNS: "translations",
+    interpolation: {
+      escapeValue: false,
+      formatSeparator: ",",
+    },
+  });
+
+export default i18n;
