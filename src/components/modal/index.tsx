30971f776eb6748d8fbdc863ee74034976ab6eae
Makkahwi <SuhaibAhmadAi@hotmail.com>
2025-06-01 17:17:16 +0300

    Build Initial Modal Comp

---
+++ src/components/modal/index.tsx
@@ -0,0 +1,43 @@
+interface Props {
+  name?: string;
+}
+
+const Modal = ({ name, ...rest }: Props) => {
+  return (
+    <div
+      className="modal fade"
+      id={name}
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h1 className="modal-title fs-5">Modal title</h1>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="modal-body">...</div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <button type="button" className="btn btn-primary">
+              Save changes
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
