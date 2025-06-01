1447d9269566d8a67345eab5b003f0d4b0856b6e
Makkahwi <SuhaibAhmadAi@hotmail.com>
2025-06-01 17:27:30 +0300

    Update Modal Comp

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,42 +1,66 @@
+import { Fragment } from "react/jsx-runtime";
+
 interface Props {
   name?: string;
+  title?: string;
+  children: React.ReactNode;
+  actions: React.ReactNode;
 }
 
-const Modal = ({ name, ...rest }: Props) => {
+const Modal = ({
+  name = "modal",
+  title,
+  children,
+  actions,
+  ...rest
+}: Props) => {
   return (
-    <div
-      className="modal fade"
-      id={name}
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true"
-    >
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h1 className="modal-title fs-5">Modal title</h1>
-            <button
-              type="button"
-              className="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div className="modal-body">...</div>
-          <div className="modal-footer">
-            <button
-              type="button"
-              className="btn btn-secondary"
-              data-bs-dismiss="modal"
-            >
-              Close
-            </button>
-            <button type="button" className="btn btn-primary">
-              Save changes
-            </button>
+    <Fragment>
+      <button
+        type="button"
+        className="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target={`#${name}`}
+      >
+        Launch demo modal
+      </button>
+
+      <div
+        className="modal fade"
+        id={name}
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+        {...rest}
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            {title && (
+              <div className="modal-header">
+                <h1 className="modal-title fs-5">{title}</h1>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                />
+              </div>
+            )}
+
+            <div className="modal-body">
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              />
+              {children}
+            </div>
+
+            <div className="modal-footer">{actions}</div>
           </div>
         </div>
       </div>
-    </div>
+    </Fragment>
   );
 };
 
