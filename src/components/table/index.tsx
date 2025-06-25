7a563c92e3316f97e3868903cf9411e6e4ada322
Makkahwi <SuhaibAhmadAi@hotmail.com>
2025-06-25 17:49:19 +0300

    Fix Table UI Responsiveness

--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -153,211 +153,213 @@ const DynamicTable = ({
   };
 
   return (
-    <table className="table mt-4 w-100">
-      <thead className="table-light">
-        <tr>
-          <th className="py-3" scope="col">
-            #
-          </th>
-
-          {columns.map(({ label }, i) => (
-            <th className="py-3 fw-bold" scope="col" key={i}>
-              {label}
-            </th>
-          ))}
-
-          {actions && actions()?.length && (
+    <div className="table-responsive">
+      <table className="table mt-4 w-100">
+        <thead className="table-light">
+          <tr>
             <th className="py-3" scope="col">
-              {t("Global.Labels.Action")}
+              #
             </th>
-          )}
-        </tr>
-      </thead>
 
-      <tbody>
-        {data.length === 0 && (
-          <tr>
-            <td colSpan={columns.length + 2} className="text-center py-4">
-              {t("Global.Labels.NoData")}
-            </td>
+            {columns.map(({ label }, i) => (
+              <th className="py-3 fw-bold" scope="col" key={i}>
+                {label}
+              </th>
+            ))}
+
+            {actions && actions()?.length && (
+              <th className="py-3" scope="col">
+                {t("Global.Labels.Action")}
+              </th>
+            )}
           </tr>
-        )}
+        </thead>
 
-        {data
-          .filter(
-            (_, i) =>
-              i >= pageSize * (pageNumber - 1) && i < pageSize * pageNumber
-          )
-          .map((row, i) => (
-            <tr key={i}>
-              <td className="py-3" scope="row">
-                {i + pageSize * (pageNumber - 1) + 1}
+        <tbody>
+          {data.length === 0 && (
+            <tr>
+              <td colSpan={columns.length + 2} className="text-center py-4">
+                {t("Global.Labels.NoData")}
               </td>
-
-              {columns.map(
-                ({ name, type, options, render, timestampFormat }, y) => (
-                  <td className="py-3" key={y}>
-                    {dataRender({
-                      row,
-                      data: (row as any)[name],
-                      type,
-                      render,
-                      options,
-                      timestampFormat,
-                    })}
-                  </td>
-                )
-              )}
-
-              {actions && actions()?.length && (
-                <td className="py-3 d-flex" scope="row">
-                  {actions(row.id)
-                    .filter(({ spread }) => spread)
-                    .map(({ icon, label, onClick }, y) => (
-                      <FontAwesomeIcon
-                        icon={icon}
-                        data-bs-toggle="tooltip"
-                        data-bs-placement="top"
-                        data-bs-custom-class="custom-tooltip"
-                        data-bs-title={label}
-                        role="button"
-                        className="me-1"
-                        onClick={() => onClick(row?.id || "")}
-                        key={y}
-                      />
-                    ))}
-
-                  {actions(row.id).filter(({ spread }) => !spread).length ? (
-                    <DropdownComp
-                      button={
-                        <FontAwesomeIcon
-                          icon={faEllipsisVertical}
-                          className="ms-1"
-                        />
-                      }
-                      list={actions(row.id)
-                        .filter(({ spread }) => !spread)
-                        .map(({ icon, label, onClick }) => ({
-                          onClick: () => onClick(row.id || ""),
-                          label: (
-                            <Fragment>
-                              {" "}
-                              <FontAwesomeIcon
-                                icon={icon}
-                                className="text-info"
-                              />{" "}
-                              {label}
-                            </Fragment>
-                          ),
-                        }))}
-                    />
-                  ) : (
-                    <span className="text-white">-</span>
-                  )}
-                </td>
-              )}
             </tr>
-          ))}
-      </tbody>
+          )}
 
-      {!noPagination && (
-        <tfoot>
-          <tr>
-            <th
-              colSpan={
-                columns.length + 1 + (actions && actions()?.length ? 1 : 0)
-              }
-            >
-              <div className="d-flex">
-                <nav className="my-auto me-2">
-                  <ul className="pagination">
-                    <li className="page-item my-auto">
-                      <button
-                        className="page-link text-info border-0 px-3"
-                        onClick={() => onPageNumberChange(pageNumber - 1)}
-                        disabled={pageNumber === 1}
-                      >
-                        <FontAwesomeIcon icon={faChevronRight} />
-                      </button>
-                    </li>
+          {data
+            .filter(
+              (_, i) =>
+                i >= pageSize * (pageNumber - 1) && i < pageSize * pageNumber
+            )
+            .map((row, i) => (
+              <tr key={i}>
+                <td className="py-3" scope="row">
+                  {i + pageSize * (pageNumber - 1) + 1}
+                </td>
 
-                    {Array(pagesCount)
-                      .fill("")
-                      .map((_, i) => (
-                        <li className="page-item my-auto" key={i}>
-                          <button
-                            className={`page-link border-0 bg-info ${
-                              pageNumber === i + 1 ? "active rounded-2" : ""
-                            }`}
-                            onClick={() => onPageNumberChange(i + 1)}
-                          >
-                            {i + 1}
-                          </button>
-                        </li>
+                {columns.map(
+                  ({ name, type, options, render, timestampFormat }, y) => (
+                    <td className="py-3" key={y}>
+                      {dataRender({
+                        row,
+                        data: (row as any)[name],
+                        type,
+                        render,
+                        options,
+                        timestampFormat,
+                      })}
+                    </td>
+                  )
+                )}
+
+                {actions && actions()?.length && (
+                  <td className="py-3 d-flex" scope="row">
+                    {actions(row.id)
+                      .filter(({ spread }) => spread)
+                      .map(({ icon, label, onClick }, y) => (
+                        <FontAwesomeIcon
+                          icon={icon}
+                          data-bs-toggle="tooltip"
+                          data-bs-placement="top"
+                          data-bs-custom-class="custom-tooltip"
+                          data-bs-title={label}
+                          role="button"
+                          className="me-1"
+                          onClick={() => onClick(row?.id || "")}
+                          key={y}
+                        />
                       ))}
 
-                    <li className="page-item my-auto">
-                      <button
-                        className="page-link text-info border-0 px-3"
-                        onClick={() => onPageNumberChange(pageNumber + 1)}
-                        disabled={pageNumber === pagesCount}
-                      >
-                        <FontAwesomeIcon icon={faChevronLeft} />
-                      </button>
-                    </li>
-                  </ul>
-                </nav>
+                    {actions(row.id).filter(({ spread }) => !spread).length ? (
+                      <DropdownComp
+                        button={
+                          <FontAwesomeIcon
+                            icon={faEllipsisVertical}
+                            className="ms-1"
+                          />
+                        }
+                        list={actions(row.id)
+                          .filter(({ spread }) => !spread)
+                          .map(({ icon, label, onClick }) => ({
+                            onClick: () => onClick(row.id || ""),
+                            label: (
+                              <Fragment>
+                                {" "}
+                                <FontAwesomeIcon
+                                  icon={icon}
+                                  className="text-info"
+                                />{" "}
+                                {label}
+                              </Fragment>
+                            ),
+                          }))}
+                      />
+                    ) : (
+                      <span className="text-white">-</span>
+                    )}
+                  </td>
+                )}
+              </tr>
+            ))}
+        </tbody>
 
-                <nav className="my-auto">
-                  <ul className="pagination">
-                    <li className="page-item my-auto">
-                      <span className="page-link border-0 d-flex">
-                        <small className="my-auto text-info">
-                          {t("Global.Labels.PageNo")}
-                        </small>
-
-                        <input
-                          value={pageNumber}
-                          className="form-control ms-1"
-                          style={{ width: "50px" }}
-                          type="number"
-                          min={1}
-                          max={pagesCount}
-                          onChange={(e) =>
-                            onPageNumberChange(parseInt(e.target.value))
-                          }
-                        />
-                      </span>
-                    </li>
-
-                    <li className="page-item my-auto">
-                      <span className="page-link border-0 d-flex">
-                        <small className="my-auto text-info">
-                          {t("Global.Labels.PageSize")}
-                        </small>
-
-                        <select
-                          value={pageSize}
-                          className="form-control ms-1"
-                          onChange={(e) =>
-                            onPageSizeChange(parseInt(e.target.value))
-                          }
+        {!noPagination && (
+          <tfoot>
+            <tr>
+              <th
+                colSpan={
+                  columns.length + 1 + (actions && actions()?.length ? 1 : 0)
+                }
+              >
+                <div className="d-flex">
+                  <nav className="my-auto me-2">
+                    <ul className="pagination">
+                      <li className="page-item my-auto">
+                        <button
+                          className="page-link text-info border-0 px-3"
+                          onClick={() => onPageNumberChange(pageNumber - 1)}
+                          disabled={pageNumber === 1}
                         >
-                          <option value={10}>10</option>
-                          <option value={20}>20</option>
-                          <option value={50}>50</option>
-                          <option value={100}>100</option>
-                        </select>
-                      </span>
-                    </li>
-                  </ul>
-                </nav>
-              </div>
-            </th>
-          </tr>
-        </tfoot>
-      )}
-    </table>
+                          <FontAwesomeIcon icon={faChevronRight} />
+                        </button>
+                      </li>
+
+                      {Array(pagesCount)
+                        .fill("")
+                        .map((_, i) => (
+                          <li className="page-item my-auto" key={i}>
+                            <button
+                              className={`page-link border-0 bg-info ${
+                                pageNumber === i + 1 ? "active rounded-2" : ""
+                              }`}
+                              onClick={() => onPageNumberChange(i + 1)}
+                            >
+                              {i + 1}
+                            </button>
+                          </li>
+                        ))}
+
+                      <li className="page-item my-auto">
+                        <button
+                          className="page-link text-info border-0 px-3"
+                          onClick={() => onPageNumberChange(pageNumber + 1)}
+                          disabled={pageNumber === pagesCount}
+                        >
+                          <FontAwesomeIcon icon={faChevronLeft} />
+                        </button>
+                      </li>
+                    </ul>
+                  </nav>
+
+                  <nav className="my-auto">
+                    <ul className="pagination">
+                      <li className="page-item my-auto">
+                        <span className="page-link border-0 d-flex">
+                          <small className="my-auto text-info">
+                            {t("Global.Labels.PageNo")}
+                          </small>
+
+                          <input
+                            value={pageNumber}
+                            className="form-control ms-1"
+                            style={{ width: "50px" }}
+                            type="number"
+                            min={1}
+                            max={pagesCount}
+                            onChange={(e) =>
+                              onPageNumberChange(parseInt(e.target.value))
+                            }
+                          />
+                        </span>
+                      </li>
+
+                      <li className="page-item my-auto">
+                        <span className="page-link border-0 d-flex">
+                          <small className="my-auto text-info">
+                            {t("Global.Labels.PageSize")}
+                          </small>
+
+                          <select
+                            value={pageSize}
+                            className="form-control ms-1"
+                            onChange={(e) =>
+                              onPageSizeChange(parseInt(e.target.value))
+                            }
+                          >
+                            <option value={10}>10</option>
+                            <option value={20}>20</option>
+                            <option value={50}>50</option>
+                            <option value={100}>100</option>
+                          </select>
+                        </span>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
+              </th>
+            </tr>
+          </tfoot>
+        )}
+      </table>
+    </div>
   );
 };
 
