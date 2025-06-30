e9c7d92383030b2f8a25b28c9763888cd32ec740
Makkahwi <SuhaibAhmadAi@hotmail.com>
2025-06-30 12:22:33 +0300

    Add Icons To Assets

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,73 @@
+import aidsIcon from "./aids.svg";
+import archiveIcon from "./archive.svg";
+import attachmentIcon from "./attachment.svg";
+import beneficiariesIcon from "./beneficiaries.svg";
+import dashboardIcon from "./dashboard.svg";
+import dateIcon from "./date.svg";
+import deleteIcon from "./delete.svg";
+import dependantIcon from "./dependant.svg";
+import deviceIcon from "./device.svg";
+import dropdownDotsIcon from "./dropdownDots.svg";
+import editIcon from "./edit.svg";
+import excelIcon from "./excel.svg";
+import filterIcon from "./filter.svg";
+import furnitureIcon from "./furniture.svg";
+import grandAidIcon from "./grandAid.svg";
+import helpIcon from "./help.svg";
+import infoIcon from "./info.svg";
+import locationIcon from "./location.svg";
+import membershipFormIcon from "./membershipForm.svg";
+import menuBarsIcon from "./menuBars.svg";
+import messagesIcon from "./messages.svg";
+import mutedStarIcon from "./mutedStar.svg";
+import notificationsIcon from "./notifications.svg";
+import pdfIcon from "./pdf.svg";
+import pickedStarIcon from "./pickedStar.svg";
+import profileIcon from "./profile.svg";
+import resetFilterIcon from "./resetFilter.svg";
+import searchIcon from "./search.svg";
+import settingsIcon from "./settings.svg";
+import successIcon from "./success.svg";
+import supportIcon from "./support.svg";
+import takePhotoIcon from "./takePhoto.svg";
+import viewIcon from "./view.svg";
+import visitReportIcon from "./visitReport.svg";
+import visitsIcon from "./visits.svg";
+
+export {
+  searchIcon,
+  visitsIcon,
+  viewIcon,
+  visitReportIcon,
+  aidsIcon,
+  archiveIcon,
+  attachmentIcon,
+  beneficiariesIcon,
+  menuBarsIcon,
+  dashboardIcon,
+  dateIcon,
+  deleteIcon,
+  dependantIcon,
+  deviceIcon,
+  dropdownDotsIcon,
+  editIcon,
+  excelIcon,
+  filterIcon,
+  furnitureIcon,
+  grandAidIcon,
+  helpIcon,
+  infoIcon,
+  locationIcon,
+  membershipFormIcon,
+  messagesIcon,
+  mutedStarIcon,
+  notificationsIcon,
+  pdfIcon,
+  pickedStarIcon,
+  profileIcon,
+  resetFilterIcon,
+  settingsIcon,
+  successIcon,
+  supportIcon,
+  takePhotoIcon,
+};
