66362e3eb81b471b9e4bb9cebae6dbe52f4fb2d7
Makkahwi <SuhaibAhmadAi@hotmail.com>
2025-07-01 15:46:10 +0300

    Build BE API-Based File Return Handler

---
+++ src/utils/downloadFiles.ts
@@ -0,0 +1,39 @@
+export const downloadFile = ({
+  response,
+  type,
+  fileName,
+}: {
+  response: any;
+  type?: string;
+  fileName?: string;
+}) => {
+  const byteCharacters = atob(response.payload);
+  const byteArrays = [];
+
+  for (let i = 0; i < byteCharacters.length; i += 512) {
+    const slice = byteCharacters.slice(i, i + 512);
+    const byteNumbers = new Array(slice.length);
+
+    for (let j = 0; j < slice.length; j++) {
+      byteNumbers[j] = slice.charCodeAt(j);
+    }
+
+    byteArrays.push(new Uint8Array(byteNumbers));
+  }
+
+  const blob = new Blob(byteArrays, {
+    type:
+      type === "pdf"
+        ? "application/pdf"
+        : "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+  });
+
+  const url = window.URL.createObjectURL(blob);
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = fileName || response.fileName;
+  document.body.appendChild(link);
+  link.click();
+  link.remove();
+  URL.revokeObjectURL(url);
+};
