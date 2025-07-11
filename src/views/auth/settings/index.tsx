b2c55a26aa14ec4d35cdac4ea534424b8182dbb5
Makkahwi <SuhaibAhmadAi@hotmail.com>
2025-07-11 12:09:58 +0300

    Centralize Setting FormInputs

--- src/views/auth/settings/index.tsx
+++ src/views/auth/settings/index.tsx
@@ -12,6 +12,11 @@ import { setFontSize, setMetadata } from "../../../store/actions/settings";
 import { useAppSelector } from "../../../store/hooks";
 import { apiCatchGlobalHandler } from "../../../utils/function";
 import AccountDelete from "./accountDelete";
+import {
+  getCommonSettingInputs,
+  getPasswordResetSettingInputs,
+  metadataSettingInputs,
+} from "../../../utils/formInputs/settings";
 
 const SettingsPage = () => {
   const { t } = useTranslation();
@@ -55,102 +60,13 @@ const SettingsPage = () => {
       .catch(apiCatchGlobalHandler);
   };
 
-  const beneficiaryInputs = () => [
-    {
-      type: "range",
-      name: "fontSize",
-      min: 10,
-      max: 25,
-      label: t("Auth.Settings.FontSize"),
-      defaultValue: 15,
-    },
-  ];
-
-  const staffInputs = (formik: FormikProps<Record<string, any>>) => {
-    const final = [
-      {
-        type: "text",
-        name: "name",
-        label: t("Auth.Settings.SocietyName"),
-      },
-      {
-        type: "file",
-        name: "logo",
-        label: t("Auth.Settings.SocietyLogo"),
-      },
-      // {
-      //   type: "selectMany",
-      //   name: "provinces",
-      //   options: [{ value: "All", label: t("Auth.Settings.AllProvinces") }, ... getProvinces(t)],
-      //   label: t("Auth.Settings.SocietyProvinces"),
-      // },
-    ];
-
-    // const allProvinces = formik?.values.provinces?.includes("All");
-    // const allGovernorate = formik?.values.governorate?.includes("All");
-    // const allCities = formik?.values.cities?.includes("All");
-
-    // if (!allProvinces) {
-    //   final.push({
-    //     type: "selectMany",
-    //     name: "governorate",
-    //     options: [{ value: "All", label: t("Auth.Settings.AllGovernorate") }],
-    //     label: t("Auth.Settings.SocietyGovernorate"),
-    //   });
-    // }
-
-    // if (!allProvinces && !allGovernorate) {
-    //   final.push({
-    //     type: "selectMany",
-    //     name: "cities",
-    //     options: [{ value: "All", label: t("Auth.Settings.AllCities") }],
-    //     label: t("Auth.Settings.SocietyCities"),
-    //   });
-    // }
-
-    // if (!allProvinces && !allGovernorate && !allCities) {
-    //   final.push({
-    //     type: "selectMany",
-    //     name: "districts",
-    //     options: [{ value: "All", label: t("Auth.Settings.AllDistricts") }],
-    //     label: t("Auth.Settings.SocietyDistricts"),
-    //   });
-    // }
-
-    final.push({
-      type: "phoneNumber",
-      name: "phoneNumber",
-      label: t("Auth.Settings.SocietyPhoneNumber"),
-    });
-
-    // final.push({
-    //   type: "location",
-    //   name: "location",
-    //   label: t("Auth.Settings.SocietyLocation"),
-    // });
-
-    // final.push({
-    //   type: "text",
-    //   name: "address",
-    //   label: t("Auth.Settings.SocietyAddress"),
-    // });
-
-    // final.push({
-    //   type: "url",
-    //   name: "website",
-    //   label: t("Auth.Settings.SocietyWebsite"),
-    // });
-
-    return final;
-  };
-
   return (
     <BoxedPage title={t("Auth.Settings.Title")}>
       <Fragment>
         <h5 className="text-info my-5">{t("Auth.Settings.System.Title")}</h5>
 
         <Form
-          inputs={beneficiaryInputs}
+          inputs={() => getCommonSettingInputs(t)}
           initialValues={{
             fontSize: fontSize,
             ...metadata,
@@ -174,22 +90,7 @@ const SettingsPage = () => {
         </h5>
 
         <Form
-          inputs={() => [
-            {
-              type: "password",
-              name: "password",
-              label: t("Public.ForgotPassword.ResetPassword.NewPassword"),
-              required: true,
-            },
-            {
-              type: "password",
-              name: "passwordConfirmation",
-              label: t(
-                "Public.ForgotPassword.ResetPassword.NewPasswordConfirmation"
-              ),
-              required: true,
-            },
-          ]}
+          inputs={() => getPasswordResetSettingInputs(t)}
           submitText={t("Global.Form.Labels.Save")}
           onFormSubmit={(values) => {
             onPasswordResetSubmit(values);
@@ -203,7 +104,7 @@ const SettingsPage = () => {
             </h5>
 
             <Form
-              inputs={staffInputs}
+              inputs={() => metadataSettingInputs(t)}
               initialValues={{
                 fontSize: fontSize,
                 ...metadata,
