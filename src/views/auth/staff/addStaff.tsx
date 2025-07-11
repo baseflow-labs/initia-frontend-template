f8f492302aa47053c30cc05bdfcba1e7741661e9
Makkahwi <SuhaibAhmadAi@hotmail.com>
2025-07-11 12:10:58 +0300

    Centralize Staff FormInputs

--- src/views/auth/staff/addStaff.tsx
+++ src/views/auth/staff/addStaff.tsx
@@ -6,6 +6,7 @@ import Form from "../../../components/form";
 import Modal from "../../../components/modal";
 import { addNotification } from "../../../store/actions/notifications";
 import { apiCatchGlobalHandler } from "../../../utils/function";
+import { getStaffCrudInputs } from "../../../utils/formInputs/staff";
 
 const AddStaff = ({
   getData,
@@ -26,40 +27,7 @@ const AddStaff = ({
       isOpen={!!openModal}
     >
       <Form
-        inputs={() => [
-          {
-            label: t("Auth.Researchers.ResearcherName"),
-            name: "name",
-            type: "text",
-            required: true,
-          },
-          {
-            label: t("Global.Form.Label.PhoneNumber"),
-            name: "username",
-            type: "phoneNumber",
-            required: true,
-          },
-          {
-            label: t("Auth.MembershipRegistration.Form.IdNumber"),
-            name: "idNumber",
-            type: "numberText",
-            minLength: 10,
-            maxLength: 10,
-            required: true,
-          },
-          {
-            label: t("Global.Form.Label.Email"),
-            name: "email",
-            type: "email",
-            required: true,
-          },
-          // {
-          //   label: t("Auth.Researchers.AddProfilePhoto"),
-          //   name: "photo",
-          //   type: "file",
-          //   required: false,
-          // },
-        ]}
+        inputs={() => getStaffCrudInputs(t)}
         submitText={t("Auth.Researchers.AddResearcher")}
         onFormSubmit={(e) => {
           ResearcherApi.create({
