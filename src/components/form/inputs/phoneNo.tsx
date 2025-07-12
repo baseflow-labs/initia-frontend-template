480866200e2aa029d287e423bf663c5bc0c598e9
Makkahwi <SuhaibAhmadAi@hotmail.com>
2025-07-12 14:10:18 +0300

    Fix Formik-Based Frontend Form Validation

--- src/components/form/inputs/phoneNo.tsx
+++ src/components/form/inputs/phoneNo.tsx
@@ -1,7 +1,8 @@
+import { useField } from "formik";
 import React from "react";
+
 import { InputProps } from "..";
 import { commonInputClasses } from "../../../utils/consts";
-import { useField } from "formik";
 
 type FinalInput = InputProps & React.InputHTMLAttributes<HTMLInputElement>;
 
@@ -15,7 +16,6 @@ const PhoneNoInput: React.FC<FinalInput> = ({ name, type, ...input }) => {
         const val = e.target.value;
         if (/^\d*$/.test(val)) helpers.setValue(val);
       }}
-      minLength={9}
       maxLength={9}
       placeholder={input.placeholder || "501234567" || input.label}
       className={`form-control form-control-sm ${commonInputClasses}`}
