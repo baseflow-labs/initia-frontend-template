8c7cccf3131440f50d5a60d2a85a193782fd985f
Makkahwi <SuhaibAhmadAi@hotmail.com>
2025-07-29 17:31:38 +0300

    build(common): file input to take multiple files

--- src/components/form/inputs/file.tsx
+++ src/components/form/inputs/file.tsx
@@ -1,14 +1,14 @@
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useField } from "formik";
-import React, { Fragment, useRef } from "react";
+import React, { useRef } from "react";
 import { useTranslation } from "react-i18next";
+import { useDispatch } from "react-redux";
 
 import { InputProps } from "..";
 import { attachmentIcon } from "../../../assets/icons/icons";
 import IconWrapperComp from "../../../assets/icons/wrapper";
 import { addNotification } from "../../../store/actions/notifications";
-import { useDispatch } from "react-redux";
 
 interface FileUploadProps {
   accept?: string;
@@ -22,13 +22,12 @@ type FinalInput = InputProps &
 const FileInput: React.FC<FinalInput> = ({
   name,
   accept,
-  fileSizeLimit = 1,
+  fileSizeLimit = 2,
   ...rest
 }) => {
   const dispatch = useDispatch();
   const { t } = useTranslation();
-  const [field, meta, helpers] = useField<File | null>(name);
-
+  const [field, meta, helpers] = useField<File[]>(name);
   const inputRef = useRef<HTMLInputElement | null>(null);
 
   const handleClick = () => {
@@ -36,45 +35,49 @@ const FileInput: React.FC<FinalInput> = ({
   };
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.currentTarget.files?.[0] || null;
+    const selectedFiles = Array.from(e.currentTarget.files || []);
 
-    if (file) {
+    const validFiles: File[] = [];
+    const rejectedFiles: File[] = [];
+
+    selectedFiles.forEach((file) => {
       const fileSizeMB = file.size / (1024 * 1024);
-
-      if (fileSizeMB > fileSizeLimit) {
-        helpers.setValue(null);
-        helpers.setError(
-          t("Global.Form.Errors.FileTooLarge", { max: fileSizeLimit })
-        );
-        dispatch(
-          addNotification({
-            type: "err",
-            msg: t("Global.Form.Errors.FileTooLarge", { max: fileSizeLimit }),
-          })
-        );
-        helpers.setTouched(true);
-
-        if (inputRef.current) {
-          inputRef.current.value = "";
-        }
-
-        return;
+      if (fileSizeMB <= fileSizeLimit) {
+        validFiles.push(file);
+      } else {
+        rejectedFiles.push(file);
       }
+    });
+
+    if (rejectedFiles.length) {
+      dispatch(
+        addNotification({
+          type: "err",
+          msg: t("Global.Form.Errors.FileTooLarge", {
+            max: fileSizeLimit,
+          }),
+        })
+      );
     }
 
-    helpers.setError(undefined);
-    helpers.setValue(file);
-  };
-
-  const handleRemoveFile = () => {
-    helpers.setValue(null);
+    if (validFiles.length > 0) {
+      const currentFiles = Array.isArray(field.value) ? field.value : [];
+      helpers.setValue([...currentFiles, ...validFiles]);
+      helpers.setTouched(true);
+    }
 
     if (inputRef.current) {
       inputRef.current.value = "";
     }
   };
 
-  const RemoveButton = () => (
+  const handleRemoveFile = (index: number) => {
+    const updatedFiles = [...(field.value || [])];
+    updatedFiles.splice(index, 1);
+    helpers.setValue(updatedFiles);
+  };
+
+  const RemoveButton = ({ index }: { index: number }) => (
     <div
       role="button"
       className="bg-danger text-white p-1 align-items-center justify-content-center text-center"
@@ -84,7 +87,7 @@ const FileInput: React.FC<FinalInput> = ({
         fontSize: 8,
         width: "20px",
       }}
-      onClick={handleRemoveFile}
+      onClick={() => handleRemoveFile(index)}
     >
       <FontAwesomeIcon icon={faXmark} className="p-0" />
     </div>
@@ -97,6 +100,7 @@ const FileInput: React.FC<FinalInput> = ({
         type="file"
         name={name}
         accept={accept}
+        multiple
         style={{ display: "none" }}
         onChange={handleFileChange}
         onBlur={field.onBlur}
@@ -107,19 +111,20 @@ const FileInput: React.FC<FinalInput> = ({
         className="btn btn-outline-success p-3 w-100 rounded-3"
         onClick={handleClick}
       >
-        {t("Global.Form.Labels.UploadAttachment")}{" "}
+        {t("Global.Form.Labels.UploadAttachments")}{" "}
         <IconWrapperComp icon={attachmentIcon} />
       </button>
 
-      <div
-        className={`mt-1 small ${field.value ? "text-muted" : "text-white"}`}
-      >
-        {field.value instanceof File ? (
-          <Fragment>
-            {field.value.name} <RemoveButton />
-          </Fragment>
+      <div className="mt-1 small text-muted">
+        {Array.isArray(field.value) && field.value.length > 0 ? (
+          field.value.map((file, index) => (
+            <div key={index} className="d-flex justify-content-between">
+              <span className="me-2">{file.name}</span>
+              <RemoveButton index={index} />
+            </div>
+          ))
         ) : (
-          "."
+          <span className="text-white">.</span>
         )}
       </div>
 
