b35ce29596289c809fbd42d02892167897ce7db8
Makkahwi <SuhaibAhmadAi@hotmail.com>
2025-08-06 15:36:06 +0300

    fix(beneficiaries): engage central accordion component into dependents

--- src/views/auth/membershipRegistration/Dependents.tsx
+++ src/views/auth/membershipRegistration/Dependents.tsx
@@ -3,13 +3,10 @@ import { useTranslation } from "react-i18next";
 import { useDispatch } from "react-redux";
 import { Fragment } from "react/jsx-runtime";
 
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { FormikProps } from "formik";
 import * as DependentApi from "../../../api/profile/dependent";
-import {
-  deleteIcon,
-  dependantIcon,
-  dependantWhiteIcon,
-} from "../../../assets/icons/icons";
-import IconWrapperComp from "../../../assets/icons/wrapper";
+import Accordion from "../../../components/accordion";
 import Button from "../../../components/core/button";
 import Spinner from "../../../components/core/spinner";
 import Form from "../../../components/form";
@@ -17,7 +14,6 @@ import { addNotification } from "../../../store/actions/notifications";
 import { useAppSelector } from "../../../store/hooks";
 import { getDependantDataInputs } from "../../../utils/formInputs/beneficiaryProfile";
 import { apiCatchGlobalHandler } from "../../../utils/function";
-import { FormikProps } from "formik";
 
 interface Props {
   customButtons: React.ReactNode;
@@ -55,117 +51,70 @@ const DependentsFormView = ({
 
   return (
     <Fragment>
-      {dependents.map((dependent: any, i: number) => (
-        <div className="accordion-item my-4" key={i}>
-          <h2 className="accordion-header mb-3" id={"heading" + String(i)}>
-            <div className="d-flex align-items-center justify-content-between ">
-              <button
-                className="btn btn-ghost p-3 w-100 text-start collapsed bg-info rounded-4 text-white"
-                data-bs-toggle="collapse"
-                data-bs-target={"#collapse" + String(i)}
-                aria-expanded="false"
-                type="button"
-                aria-controls={"collapse" + String(i)}
-                onClick={() => {
-                  setActiveCollapse(
-                    activeCollapse === i ? dependent.length : i
-                  );
-                }}
-              >
-                <IconWrapperComp icon={dependantWhiteIcon} className="me-2" />{" "}
-                {dependent.fullName ||
-                  t("Auth.MembershipRegistration.Form.Dependents.Dependant") +
-                    " " +
-                    (i + 1)}
-              </button>
+      <Accordion
+        data={dependents.map((dependent, i) => ({
+          header:
+            dependent.fullName ||
+            t("Auth.MembershipRegistration.Form.Dependents.Dependant") +
+              " " +
+              (i + 1),
+          body: (
+            <Form
+              inputs={dependentsDataInputs}
+              submitText={
+                t("Auth.MembershipRegistration.Form.Dependents.SaveDependent") +
+                " " +
+                (dependent.fullName || i + 1)
+              }
+              initialValues={dependent}
+              onFormSubmit={(e) => {
+                DependentApi.createOrUpdate({
+                  beneficiary,
+                  ...e,
+                })
+                  .then(() => {
+                    setActiveCollapse(dependents.length);
 
-              <Button
-                color="ghost"
-                text="danger"
-                size="sm"
-                type="button"
-                className="border border-1 rounded-4 py-3 ms-2 px-3"
-                onClick={() => remove(i)}
-              >
-                <IconWrapperComp icon={deleteIcon} />
-              </Button>
-            </div>
-          </h2>
+                    dispatch(
+                      addNotification({
+                        msg: t(
+                          "Auth.MembershipRegistration.Form.Dependents.DependentSaved",
+                          { name: e.fullName }
+                        ),
+                      })
+                    );
 
-          <div
-            id={`collapse${i}`}
-            className={`accordion-collapse collapse ${
-              activeCollapse === i ? "show" : ""
-            }`}
-            aria-labelledby={`heading${i}`}
-          >
-            <div className="accordion-body">
-              <Form
-                inputs={dependentsDataInputs}
-                submitText={
-                  t(
-                    "Auth.MembershipRegistration.Form.Dependents.SaveDependent"
-                  ) +
-                  " " +
-                  dependent.fullName
-                }
-                initialValues={dependent}
-                onFormSubmit={(e) => {
-                  DependentApi.createOrUpdate({
-                    beneficiary,
-                    ...e,
+                    const data = [...dependents, e]
+                      .filter((d) => d.idNumber)
+                      .reverse()
+                      .reduce(
+                        (final, data) =>
+                          final.find((f: any) => f.idNumber === data.idNumber)
+                            ? final
+                            : [...final, data],
+                        []
+                      )
+                      .reverse();
+
+                    setDependents(data);
+                    saveData(data);
                   })
-                    .then(() => {
-                      setActiveCollapse(dependents.length);
-
-                      dispatch(
-                        addNotification({
-                          msg: t(
-                            "Auth.MembershipRegistration.Form.Dependents.DependentSaved",
-                            { name: e.fullName }
-                          ),
-                        })
-                      );
-
-                      const data = [...dependents, e]
-                        .filter((d) => d.idNumber)
-                        .reverse()
-                        .reduce(
-                          (final, data) =>
-                            final.find((f: any) => f.idNumber === data.idNumber)
-                              ? final
-                              : [...final, data],
-                          []
-                        )
-                        .reverse();
-
-                      setDependents(data);
-                      saveData(data);
-                    })
-                    .catch(apiCatchGlobalHandler);
-                }}
-              />
-            </div>
-          </div>
-        </div>
-      ))}
-
-      <Button
-        color="success"
-        outline
-        type="button"
-        className="my-4"
-        onClick={() => {
-          setActiveCollapse(dependents.length);
+                  .catch(apiCatchGlobalHandler);
+              }}
+            />
+          ),
+        }))}
+        key="dependents"
+        icon={faUser}
+        onAdd={() =>
           setDependents((current) => [
             ...current,
             { fullName: "", idNumber: "" },
-          ]);
-        }}
-      >
-        {t("Auth.MembershipRegistration.Form.Dependents.AddNew")}{" "}
-        <IconWrapperComp icon={dependantIcon} />
-      </Button>
+          ])
+        }
+        addText={t("Auth.MembershipRegistration.Form.Dependents.AddNew")}
+        onRemove={(i) => remove(i)}
+      />
 
       {customButtons}
 
