406e477b42af441d133fcc0d2acc4c02b2e59232
Makkahwi <SuhaibAhmadAi@hotmail.com>
2025-09-15 10:37:25 +0300

    fix(common): sidebar version number

--- src/layouts/common/copyright.tsx
+++ src/layouts/common/copyright.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from "react-i18next";
 
 import Logo from "../../assets/images/brand/logo-full.png";
 import LogoOnly from "../../assets/images/brand/logo-only.png";
+import { APP_VERSION } from "../../documentation/version";
 
 const CopyRightView = ({
   oneLine,
@@ -23,6 +24,10 @@ const CopyRightView = ({
           <img src={short ? LogoOnly : Logo} height="65" alt="Logo" />
         </a>
       )}
+
+      <div className={oneLine ? "" : "mt-3 mb-2"}>
+        <small className="text-muted">V.{APP_VERSION}</small>
+      </div>
 
       {oneLine ? (
         ""
