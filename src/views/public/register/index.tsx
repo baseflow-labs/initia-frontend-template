ba19a6d29f4606bd4a29b996f4576c2dfd61f431
Makkahwi <SuhaibAhmadAi@hotmail.com>
2025-11-03 14:59:49 +0300

    fix(code): of registration page

--- src/views/public/register/index.tsx
+++ src/views/public/register/index.tsx
@@ -1,65 +1,26 @@
-import { FormikErrors } from "formik";
-import { Fragment, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router";
 
 import * as authApi from "../../../api/auth";
-import BelowInputButton from "../../../components/button/belowInput";
 import Form from "../../../components/form";
+import { login } from "../../../store/actions/auth";
 import { addNotification } from "../../../store/actions/notifications";
 import { apiCatchGlobalHandler } from "../../../utils/function";
 
 const RegisterView = () => {
   const { t } = useTranslation();
-  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const [data, setData] = useState({ username: "" });
 
-  const onRegisterSubmit = (values = { username: "" }) => {
-    authApi
-      .otpSend(values.username)
-      .then(() => {
-        setData(values);
-        dispatch(
-          addNotification({
-            msg: t("Global.Form.SuccessMsg", {
-              action: t("Public.ForgotPassword.SendOtp.SendOtp"),
-              data: "966" + values.username,
-            }),
-          })
-        );
-      })
-      .catch(apiCatchGlobalHandler);
-  };
-
-  const onOtpSubmit = (values: authApi.registerProps) => {
-    authApi
-      .register({ ...values, ...data })
-      .then(() => {
-        dispatch(addNotification({ msg: t("Public.Register.Labels.Success") }));
-        navigate("/");
-      })
-      .catch(apiCatchGlobalHandler);
-  };
-
-  const registerInputs = () => [
-    {
-      type: "text",
-      name: "name",
-      label: t("Public.Register.Labels.Name"),
-      required: true,
-    },
+  const formInputs = () => [
     {
       type: "phoneNumber",
-      name: "username",
+      name: "identifier",
       label: t("Public.Register.Labels.PhoneNo"),
       required: true,
     },
     {
       type: "password",
       name: "password",
-      minLength: 8,
       label: t("Public.Register.Labels.Password"),
       required: true,
     },
@@ -68,63 +29,32 @@ const RegisterView = () => {
       name: "passwordConfirmation",
       label: t("Public.Register.Labels.PasswordConfirmation"),
       required: true,
-      belowComp: (
-        <BelowInputButton
-          introText={t("Public.Register.Labels.ByClickYouAccept")}
-          buttonText={t("Public.Register.Labels.PrivacyPolicyTermsConditions")}
-          route="/terms-conditions"
-        />
-      ),
     },
   ];
 
-  const otpInputs = () => [
-    {
-      type: "otp",
-      name: "code",
-      required: true,
-      belowComp: (
-        <BelowInputButton
-          introText={t("Public.Register.Labels.DidNotGetOtp")}
-          buttonText={t("Public.Register.Labels.ResendOtp")}
-          action={() => onRegisterSubmit(data)}
-        />
-      ),
-    },
-  ];
-
-  const validatePasswords = (values: Record<string, any>) => {
-    const errors: FormikErrors<Record<string, any>> = {};
-    if (values.password !== values.passwordConfirmation) {
-      errors.passwordConfirmation = t("Global.Form.Errors.PasswordMatch");
-    }
-    return errors;
+  const onSubmit = (values: authApi.registerProps) => {
+    authApi
+      .register(values)
+      .then((res: any) => {
+        dispatch(
+          addNotification({
+            msg: t("Public.Register.Labels.Success", {
+              name: res.payload.user.name,
+            }),
+          })
+        );
+        dispatch(login(res.payload));
+      })
+      .catch(apiCatchGlobalHandler);
   };
 
   return (
     <div>
-      {data.username ? (
-        <Fragment>
-          <h4>{t("Public.Otp.Title")}</h4>
-
-          <div className="text-center mt-2">
-            <small>{t("Public.Otp.CheckSms")}</small>
-          </div>
-
-          <Form
-            inputs={otpInputs}
-            submitText={t("Public.ForgotPassword.SendOtp.ConfirmOTP")}
-            onFormSubmit={onOtpSubmit}
-          />
-        </Fragment>
-      ) : (
-        <Form
-          inputs={registerInputs}
-          submitText={t("Public.Register.Labels.Register")}
-          onFormSubmit={onRegisterSubmit}
-          customValidate={validatePasswords}
-        />
-      )}
+      <Form
+        inputs={formInputs}
+        submitText={t("Public.Register.Labels.Register")}
+        onFormSubmit={onSubmit}
+      />
     </div>
   );
 };
