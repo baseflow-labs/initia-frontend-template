cd51ec3803d6bb84b4a570eb995bf212e3ab51c5
Makkahwi <SuhaibAhmadAi@hotmail.com>
2025-11-03 18:10:37 +0300

    fix(code): minor issue

--- src/views/auth/users/inputs.ts
+++ src/views/auth/users/inputs.ts
@@ -1,5 +1,35 @@
 import { renderDataFromOptions } from "../../../utils/function";
-import { getUserRoles } from "../../../utils/optionDataLists/users";
+
+export const getUserRoles = (t: Function) => [
+  {
+    value: "admin",
+    label: t("Global.Labels.Roles.admin"),
+  },
+  {
+    value: "ceo",
+    label: t("Global.Labels.Roles.ceo"),
+  },
+  {
+    value: "accountant",
+    label: t("Global.Labels.Roles.accountant"),
+  },
+  {
+    value: "hod",
+    label: t("Global.Labels.Roles.hod"),
+  },
+  {
+    value: "researcher",
+    label: t("Global.Labels.Roles.researcher"),
+  },
+  {
+    value: "user",
+    label: t("Global.Labels.Roles.user"),
+  },
+  {
+    value: "applicant",
+    label: t("Global.Labels.Roles.applicant"),
+  },
+];
 
 export const inputs = (t: Function) => [
   {
