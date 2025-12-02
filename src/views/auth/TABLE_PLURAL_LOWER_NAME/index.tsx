68623ce4b0194c7c3fa2e27b54b5e1c40b3ed4fb
Makkahwi <SuhaibAhmadAi@hotmail.com>
2025-12-02 13:40:37 +0300

    build(common): example view for data table page

---
+++ src/views/auth/TABLE_PLURAL_LOWER_NAME/index.tsx
@@ -0,0 +1,33 @@
+import { Fragment } from "react";
+import { useTranslation } from "react-i18next";
+
+import DemoLoginNote from "../../../layouts/auth/demoLoginNote";
+import TablePage from "../../../layouts/auth/pages/tablePage";
+import { inputs } from "./inputs";
+
+const TABLE_PLURAL_UPPER_NAMEView = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Fragment>
+      <DemoLoginNote />
+
+      <TablePage
+        title={t("Auth.TABLE_PLURAL_UPPER_NAME.Title")}
+        // actionButtons={actionButtons}
+        columns={inputs(t)}
+        // searchProp="name"
+        // searchPlaceholder={t("Auth.TABLE_PLURAL_UPPER_NAME.SearchBarPlaceholder")}
+        // tableExtraActions={(id?: string) => [
+        //   {
+        //     label: t("Common.Edit"),
+        //     icon: faEdit
+        //   },
+        // ]}
+        dataApiEndpoint="TABLE_PLURAL_UPPER_NAME"
+      />
+    </Fragment>
+  );
+};
+
+export default TABLE_PLURAL_UPPER_NAMEView;
