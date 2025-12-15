edde195e003b4e8ff1b2ebdc05f3f70f6c1c2972
Makkahwi <SuhaibAhmadAi@hotmail.com>
2025-12-15 11:35:53 +0300

    fix(code): nested button error

--- src/components/button/lang.tsx
+++ src/components/button/lang.tsx
@@ -6,20 +6,16 @@ import { faLanguage } from "@fortawesome/free-solid-svg-icons";
 const LangButton = () => {
   const { i18n } = useTranslation();
 
-  const languages = (i18n.options.supportedLngs || []).filter((lang = '') => lang !== "cimode").map((lang = "") => (
-    {
-      label: lang === 'ar' ? 'العربية' : lang.toUpperCase(),
-      onClick: () => i18n.changeLanguage(lang)
-    }
-  ));
+  const languages = (i18n.options.supportedLngs || [])
+    .filter((lang = "") => lang !== "cimode")
+    .map((lang = "") => ({
+      label: lang === "ar" ? "العربية" : lang.toUpperCase(),
+      onClick: () => i18n.changeLanguage(lang),
+    }));
 
   return (
     <DropdownComp
-      button={
-        <button className="btn btn-link py-auto">
-          <FontAwesomeIcon icon={faLanguage} className="text-primary" />
-        </button>
-      }
+      button={<FontAwesomeIcon icon={faLanguage} className="text-primary" />}
       list={languages}
     />
   );
