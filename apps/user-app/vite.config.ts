0d9b5c88cb8c6cba9626987f984a54d300917a82
Makkahwi <SuhaibAhmadAi@hotmail.com>
2026-01-29 10:44:51 +0300

    build(common): apps and packages new structure

--- apps/user-app/vite.config.ts
+++ apps/user-app/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
-      "@initia/shared": path.resolve(__dirname, "../shared/src"),
+      "@initia/shared": path.resolve(__dirname, "../../packages/shared/src"),
     },
   },
 });
